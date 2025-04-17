baca15d92b66988f04118658729427f9cc63caab
svylabs <sg@svylabs.com>
2025-04-17 18:08:23 +0200

    Adding networks section

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks:{
+    hardhat: {
+        accounts: {
+            count: 500, // Adjust this number for hundreds of accounts
+            accountsBalance: "1000000000000000000000" // 1000 ETH per account
+        }
+    }
+  }
 };
 
 export default config;
